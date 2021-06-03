5add0b94d30544ae917c70ea2f0d6d4c98d1b34b
Recai Oktaş <roktas@gmail.com>
2021-06-03 15:38:20 +0300

    refactor: Move path.volatile to debian

--- src/all/path/idantic.sh
+++
@@ -1,6 +0,0 @@
-path.idantic() {
-	local actual=${1?${FUNCNAME[0]}: missing argument};   shift
-	local expected=${1?${FUNCNAME[0]}: missing argument}; shift
-
-	[[ $(realpath -m "$actual") = $(realpath -m "$expected") ]]
-}
