c7704540b33e5f9ee15f088d026376072c347150
Recai Oktaş <roktas@gmail.com>
2021-06-03 22:38:31 +0300

    fix(all): Fix marker

--- src/all/text/fix.sh
+++ src/all/text/fix.sh
@@ -2,12 +2,12 @@ text.fix() {
 	local file=${1?${FUNCNAME[0]}: missing argument}; shift
 	local mark=${1:-_}
 
-	[[ ! -t 0   ]] || die 'Input from stdin required'
+	[[ ! -t 0   ]] || die 'No data available on stdin'
 	[[ -f $file ]] || die "No such file: $file"
 
 	text.unfix "$file" "$mark"; {
 		echo "# begin $mark"
 		cat
-		echo "# END $mark"
+		echo "# end $mark"
 	} >>"$file"
 }
