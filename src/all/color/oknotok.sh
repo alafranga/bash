4c365282fb3044bf76af6de8e83c7c9f7a7c6244
Recai Oktaş <roktas@gmail.com>
2021-08-22 12:36:00 +0300

    feat(color): Add missing functions

--- src/all/color/oknotok.sh
+++ src/all/color/oknotok.sh
@@ -1,4 +1,14 @@
 oknotok() {
-	echo -ne "\e[1m\e[38;5;14m>\e[0m \e[1m$1\e[0m" >&2
-	"${@:2}" && echo -e "\e[1m\e[38;5;10m✓\e[0m" >&2 || echo -e "\e[1m\e[38;5;9m✗\e[0m" >&2
+	local err=0
+
+	echo -ne "\e[1m\e[38;5;6m$1\e[0m " >&2
+	"${@:2}" || err=$?
+
+	if [[ $err -eq 0 ]]; then
+		echo -e "\e[1m\e[38;5;10m✓\e[0m" >&2
+	else
+		echo -e "\e[1m\e[38;5;9m✗\e[0m" >&2
+	fi
+
+	return $err
 }
